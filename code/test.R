8f05fe7ba01fd5c2c7c909aae3fcb1e8308b6a0f
sceriff0 <valeriofassi@hotmail.it>
2026-07-05 16:27:47 +0200

    :sparkles: added data loaders

--- code/test.R
+++ code/test.R
@@ -2,10 +2,12 @@ library(tidyverse)
 library(DESeq2)
 library(here)
 library(readxl)
+library(fs)
+library("data.table")
 
 dds <- get(load(here("data", "counts.RData")))
 dds <- DESeq(dds)
-dds <- estimateSizeFactors(dds) 
+dds <- estimateSizeFactors(dds)
 
 clinical_data <- read_excel(here("data", "clinical_data.xlsx")) |>
   filter(!is.na(`ID PATIENT`)) |>
@@ -21,13 +23,41 @@ neoplastic_data <- tribble(
   "103382",              75,            NA,            NA
 )
 
-
 counts_data <-  counts(dds, normalized = TRUE) |>
-  as_tibble(rownames = "GENE") |> 
+  as_tibble(rownames = "GENE") |>
   pivot_longer(cols = -GENE, names_to = "Sample", values_to = "Expression") |>
   pivot_wider(names_from = GENE, values_from = Expression) |>
   filter(Sample %in% clinical_data$`ID CRF PRESERVE`)
 
+process_patient <- function(csv_path) {
 
+  patient_id <- path_ext_remove(path_file(csv_path))   # patient1.csv -> patient1
 
+  message(sprintf("LOADING PATIENT: %s", patient_id))
+
+  ihc_data <- fread(csv_path) |> as_tibble()
+
+  ihc_clean_data <- ihc_data |>
+    mutate(
+      phenotype_clean = str_extract(phenotype, "(?<=\\().*?(?=\\))"),
+      patient_id      = patient_id
+    )
+
+  ihc_clean_data
+}
+
+load_ihc_data <- function() {
+
+  csv_paths  <- dir_ls("~/ihc_method/data/flowpath", glob = "*.csv")   # point this at folder X
+  safe_process <- possibly(process_patient, otherwise = NULL, quiet = FALSE)
+
+  map(csv_paths, safe_process) |> list_rbind()
+}
+
+ihc_data <- load_ihc_data()
+
+colnames(clinical_data)
+colnames(neoplastic_data)
+colnames(counts_data)
+colnames(ihc_data)
 
